41d7fa06f43c088a3632e85de0e6f4febb19cd78
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-07-07 11:50:34 +0530

    Feat: Window styling

---
+++ src/Components/Window/Window.js
@@ -0,0 +1,14 @@
+import "./window.css";
+
+const Window = () => {
+  return (
+    <div className="window">
+      <div className="floating-block">
+        <h3>Floating..</h3>
+        <h3>Drag me around..</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Window;
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,10 +1,12 @@
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import Window from "../../Components/Window/Window";
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <Header />
+      <Window />
       <Footer />
     </div>
   );
